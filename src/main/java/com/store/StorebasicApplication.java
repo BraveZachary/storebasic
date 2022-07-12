25fdeda441073253e2a0500257aedb0e99a48b65
BraveZachary <mario.veas02@gmail.com>
2022-07-11 22:09:31 -0400

    primer commit

---
+++ src/main/java/com/store/StorebasicApplication.java
@@ -0,0 +1,13 @@
+package com.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorebasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorebasicApplication.class, args);
+	}
+
+}
